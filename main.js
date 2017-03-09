9962cf5833bd824efa8607559c1bf95ec6c1db85
jennvance <vancejenniferm@gmail.com>
2017-03-08 20:44:40 -0500

    adding multiple cts

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+var total = 0;
+
 document.addEventListener("submit", function(e){
 	e.preventDefault()
 	var form = e.target
 	var formData = new FormData(form)
-	formData.append('wdct', 'wordct')
+	formData.append('numwds', 'numwds')
 	formData.append('date', 'date')
+	var givenCt = parseInt(formData.get('numwds'))
 	var givenDate = formData.get("date")
 	console.log(givenDate)
+	console.log(givenCt)
+	totalCt(givenCt)
+	//put in callback later
+	document.getElementById("wordct").reset()
 
 })
+
+function totalCt(givenCt){
+	total += givenCt;
+	console.log(total)
+
+}
+

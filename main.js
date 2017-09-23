f4764fa9eda264c7c181e3dab1efba45a691fc88
jennvance <vancejenniferm@gmail.com>
2017-09-22 18:32:38 -0600

    added calculations

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ function Day(date, numwords){
 
 var allCounts = [];
 
-document.addEventListener("submit", function(e){
+//insert dummy data for testing
+// var day1 = new Day()
+
+document.querySelector('#wordct').addEventListener("submit", function(e){
 	e.preventDefault()
 	var form = e.target
 	var formData = new FormData(form)
 	formData.append('numwds', 'numwds')
 	formData.append('date', 'date')
 	submitEntry(formData)
-	//put in callback later
 	document.getElementById("wordct").reset()
 })
 
@@ -35,3 +37,49 @@ function submitEntry(formData){
 	}
 }
 
+document.querySelector('#monthform select').addEventListener('change', function(event){
+	// daily average for the month 
+	var monthVal = parseInt(document.getElementById("selectmonth").value)
+	// return new Date(mo.getFullYear(), mo.getMonth()+1, 0).getDate();
+	var daysInMonth = new Date(2017, monthVal+1, 0).getDate();
+	// Calculate and display monthly total 
+	calcTotalMonth(daysInMonth)
+	// most productive date of the month
+
+	// most productive day of the week for that month
+
+})
+
+
+
+function calcTotalMonth(numDays) {
+	var monthtotal = 0;
+	for(var i=0; i<allCounts.length; i++){
+		//alternative: parse month digit from date string
+		//actual: convert date string to date object, call method
+		var mo = new Date(allCounts[i].date)
+		var datamonth = mo.getMonth()
+		var selectmonth = parseInt(document.getElementById("selectmonth").value)
+		if(datamonth === selectmonth) {
+			monthtotal += allCounts[i].numwords;
+		}
+	}
+	renderCalcs(monthtotal);
+	var avg = monthtotal / numDays;
+	console.log(avg)
+}
+
+
+
+function findProductiveDate(){
+
+}
+
+function findProductiveDay(){
+
+}
+
+function renderCalcs(mT){
+	var moTotal = document.getElementById('totalmonth')
+	moTotal.innerHTML = mT
+}

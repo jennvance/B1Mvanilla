e8be62f96f1e3c8bd6158b0d0cda27e0b19a9832
jennvance <vancejenniferm@gmail.com>
2017-09-23 23:39:29 -0600

    calculate all time daily average

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function Day(date, numwords){
 var total = 0;
 var allCounts = [];
 
+
 document.querySelector('#wordct').addEventListener("submit", function(e){
 	e.preventDefault()
 	var form = e.target
@@ -114,13 +115,29 @@ function renderCalcs(mT, avg){
 }
 
 function calcAllTimeAverage(){
-	//First need to sort array by date...
-	console.log(allCounts)
-	//Find first date you entered a count
-
-	//Calculate number of days since then
-
+	//First need to sort array by date
+	allCounts.sort(function(a,b){
+		return new Date(a.date) - new Date(b.date)
+	})
+	//Calculate number of days since your first count
+	var msPerDay = 1000*60*60*24
+	var today = new Date();
+	//dayOne is initializing to 6 hours prior to 00:00 UTC on correct date
+	//diff calculation makes more real life sense with bug. refactor later.
+	var dayOne = new Date(allCounts[0].date);
+	var diff = diffDates(dayOne, today)
+	function diffDates(a,b){
+		//is this even working as expected?
+		var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate())
+		var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate())
+		console.log("utc1: " + utc1 + " utc2: " + utc2)
+		return Math.floor((utc2 - utc1) / msPerDay)
+	}
 	//Divide total words by days since started
+	var allTimeAvg = (total / diff).toFixed(0)
+	//render calculation
+	var dailyAvg = document.getElementById('dailyAvg')
+	dailyAvg.innerHTML = allTimeAvg
 }
 
 function calcWritingDaysOnlyAvg(){

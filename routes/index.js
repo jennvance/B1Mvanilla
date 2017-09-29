28ced8d04809d482c4f463e5d0c2cc2f38bd7dde
jennvance <vancejenniferm@gmail.com>
2017-09-28 18:30:48 -0600

    logo animation, styles, routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// still need to create html templates for each route
+
+// router.get('/onWordCountSubmit', function(req, res){
+// 	res.render('', { title: 'Stats'})
+// })
+
+// router.get('/setaGoal', function(req, res){
+// 	res.render('', { title: 'SetaGoal'})
+// })
+
+// router.get('/onWordCountSubmit', function(req, res){
+// 	res.render('', { title: 'Stats'})
+// })
+
+
+
+
+
+
 module.exports = router;

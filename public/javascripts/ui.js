73e77ee62b3ecc2e8dda99d13532d6691eecd9f3
jennvance <vancejenniferm@gmail.com>
2017-11-01 14:13:19 -0600

    finished login modal

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+	$("#li-button").on("click", function(e){
+		e.preventDefault()
+		$("#signup-form").hide()
+		$("#login-form").show()
+		$("#li-button").hide()
+		$("#su-button").show()
+	})
+	$("#su-button").on("click", function(e){
+		e.preventDefault()
+		$("#login-form").hide()
+		$("#signup-form").show()
+		$("#su-button").hide()
+		$("#li-button").show()
+	})
+	$("#overlay, #x").on("click", function(e){
+		e.preventDefault()
+		$("#overlay").hide();
+	})
+
+	$(".su-panel").on("click", function(e){
+		e.stopPropagation()
+		e.preventDefault()
+	})
 
 })

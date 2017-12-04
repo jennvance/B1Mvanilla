f5da1e70c568324c188d8d324f9e4b2e7300a0c2
jennvance <vancejenniferm@gmail.com>
2017-12-04 11:08:32 -0700

    basic badge schema

---
+++ dbmodels/badgemodels.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+
+var BadgeSchema = new mongoose.Schema({
+	title: String,
+	summary: String,
+	img: String
+})
+
+var BadgeModel = mongoose.model('Badge', BadgeSchema)
+
+module.exports = BadgeModel
